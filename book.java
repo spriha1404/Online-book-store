77d73fb320420b0bf22fd2b30548ffee09595944
spriha1404 <92588120+spriha1404@users.noreply.github.com>
2024-09-10 12:04:00 +0530

    Update book.java

---
+++ book.java
@@ -0,0 +1,29 @@
+lic class Book {
+    private int bookId;
+    private String title;
+    private String author;
+    private double price;
+
+    public Book(int bookId, String title, String author, double price) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
